bb426b61d54c8fcf3389c97bc11e2ee88292e5da
syCHEN1645 <mreredast@gmail.com>
2025-09-10 16:41:17 +0800

    Added naming capability in calibrate.py - yan xu

--- calibrate.py
+++ calibrate.py
@@ -11,19 +11,63 @@ def main():
     camera_index_list = scanActiveCameras()
     # clibrate each camera
     # possibly show picture from cameras to confirm
+import json
+import os
+import sys
+import cv2
 
+from config import CONFIG_CALIBRATION_PATH
+from auto_process import scanActiveCameras
+
+def main():
+    # todo: scan cameras
+    # todo: get a proper list of cam names
+    camera_index_list = scanActiveCameras()
+    # clibrate each camera
+    # show picture from cameras to confirm
     config = []
     # todo: loop this for each cam
     for index in camera_index_list:
+        print("Taking a photo and displaying in imageviewer...")
+        # todo: open and display photo
+        cam = cv2.VideoCapture(index)
+        if not cam.isOpened():
+            print("Camera error, please re-run the programme")
+            return
+
+        ret, frame = cam.read()
+        if ret:
+            try:
+                cv2.imshow(f"{index}", frame)
+                cv2.waitKey(1000)
+            except Exception:
+                print("Failed to show image.")
+
+        cam.release()
+
+        print(f"Key in name for camera {index}: ")
+        camera_name = (str)(input())
+        print(f"Key in wellhead name for camera {index}: ")
+        wellhead_name = (str)(input())
+        print(f"Key in gauge name for camera {index}: ")
+        gauge_name = (str)(input())
         print(f"Key in start point value for camera {index}: ")
         start_marking = (float)(input())
         print(f"Key in end point value for camera {index}: ")
         end_marking = (float)(input())
         print(f"Key in unit for camera {index}: ")
         unit = (str)(input())
+        try:
+            cv2.destroyWindow(f"{index}")
+            print("Closing image")
+        except Exception:
+            print("Image already closed")
 
         config.append(
             {
+            "camera_name": camera_name,
+            "wellhead_name": wellhead_name,
+            "gauge_name": gauge_name,
             "index": index,
             "start_marking": start_marking,
             "end_marking": end_marking,
@@ -48,5 +92,5 @@ def main():
         print("Quit")
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
